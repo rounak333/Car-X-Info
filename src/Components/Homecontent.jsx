2b63916c9858b35404496ef2cc01c9662aea26c6
Rounak Choubey <166724724+rounak333@users.noreply.github.com>
2024-09-26 22:17:10 +0530

    Update Homecontent.jsx

--- src/Components/Homecontent.jsx
+++ src/Components/Homecontent.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import AOS styles
-import NavLink from
+import { NavLink } from 'react-router-dom';
 
 const HomeContent = () => {
   useEffect(() => {
